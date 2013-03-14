ebdd89c4bf11c729d7aed2a33ff4bc3a36950d11
retoj <retoj@67a639a9-6c73-4e1c-95b4-e966455b0fe7>
2013-03-14 19:48:20 +0000

    read post request parameters from $POST array

--- giusi/web/update.php
+++ giusi/web/update.php
@@ -5,6 +5,14 @@ include "kalender.php";
 $link = getDB();
 
 // update
+
+$woche = $_POST['woche'];
+$text = $_POST['text'];
+$naechte = $_POST['naechte'];
+$jahr = $_POST['jahr'];
+
+// echo "woche=$woche, text=$text, naechte=$naechte, jahr=$jahr";
+
 if (isset($woche)) {
 	$result = mysql_query ("UPDATE giusiwochen SET text='$text', naechte='$naechte' WHERE woche='$woche' AND jahr='$jahr'", $link);
 	if (!$result) {
