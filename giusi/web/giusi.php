de97e67d7bf146397ff65884f4422acfc9a4da1e
retoj <retoj@67a639a9-6c73-4e1c-95b4-e966455b0fe7>
2017-01-25 04:45:39 +0000

    kalender 2017 aufgeschaltet

--- giusi/web/giusi.php
+++ giusi/web/giusi.php
@@ -3,7 +3,7 @@
 <?php 
 function printHistory() {
 	$jahrFirst = 2006;
-	$jahrLast = 2015;
+	$jahrLast = 2016;
 	for ($jahr = $jahrLast; $jahr >= $jahrFirst; $jahr--) {
 		?>
 		<a href="history.php?jahr=<?php print $jahr ?>">Belegungsplan f&uuml;r das Jahr <?php print $jahr ?></a>
@@ -21,14 +21,11 @@ function printHistory() {
 	</head>
 	<body>
 		
-		<?php printYear(2016, true, true, "giusi.php"); ?>
+		<?php printYear(2017, true, true, "giusi.php"); ?>
 		
 		<!-- vergangene und zuk&uuml;nftige Jahre -->
 		<div id="history">
 		    <b>
-		        <p>
-		            <a href="history.php?jahr=2017">*** Neuer Belegungsplan für 2017 ***</a>
-		        </p>
 			    <?php printHistory() ?>
 			</b>
 		</div>
